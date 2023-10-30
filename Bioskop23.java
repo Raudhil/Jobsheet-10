29b8e6b2aba9e054f84c4fd03389639dd00a9499
Raudhil <raudhilfirdausn@gmail.com>
2023-10-30 09:59:59 +0700

    For Each loop untuk menampilkan panjang baris

--- Bioskop23.java
+++ Bioskop23.java
@@ -12,14 +12,12 @@ public class Bioskop23 {
         penonton[3][0] = "Gisel";
         penonton[3][1] = "Hana";
 
-
-        System.out.printf("%s \t %s \n", penonton[0][0],penonton[0][1]);
-        System.out.printf("%s \t %s \n", penonton[1][0],penonton[1][1]);
-        System.out.printf("%s \t %s \n", penonton[2][0],penonton[2][1]);
-        System.out.printf("%s \t %s \n", penonton[3][0],penonton[3][1]);
-
         for (int i = 0 ; i < penonton.length ; i++){
             System.out.println("Panjang baris ke-" + (i + 1) + ": " + penonton[i].length);
+        }
+
+        for (String[] barisPenonton : penonton){
+            System.out.println("Panjang baris: " + barisPenonton.length);
         }
     }
 }
